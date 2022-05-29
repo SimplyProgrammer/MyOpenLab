46db587dab11856e768301979b7648d7e263be2c
PetoPetko <petko.hupka@gmail.com>
2022-05-29 21:41:47 +0200

    trying to fix mobile responsibility issues...

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
 
 	<Z-main-navbar v-bind="nav">
 		<template #sideMenu>
-			<div class="nav-side-menu position-absolute">
+			<div class="nav-side-menu">
 				<p class="side-header-text mx-5">Ak máš záujem študovať spolu s nami, sme tu pre teba…</p>
 				<a href="https://discord.com/invite/Y6xrdTvjFn" class="join-discord-btn mx-3 btn btn-sm rounded-pill d-flex justify-content-center align-items-center">OPENLAB COMMUNITY
 					<img class="ms-1" :src="origAssets + 'images/discord.svg'" alt="discord">
